5c6449395411584e2355f3b6e46bdd5f69b63a7e
ridhamu <ridhauwu@gmail.com>
2025-06-01 23:30:58 +0700

    done while loop, next on for loop and function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
     println!("Hello, world!");
+
+
+    // here list all the non-unit test function to avoid annoying warning 
+    function_a(); 
+    function_b(); 
+
+
+    // all global scope var to avoid warning
+    println!("{}", ANOTHER_ANOTHER_NUMBER); 
+    println!("{}", GLOBAL_SCOPE); 
 }
 
 #[test]
@@ -105,13 +115,236 @@ fn constant() {
 const GLOBAL_SCOPE: i32 = 1;
 #[test]
 fn variable_scope() {
-    println!("{}", GLOBAL_SCOPE);
+     println!("{}", GLOBAL_SCOPE);
 
     let local_scope: i32 = 2;
     println!("{}", local_scope);
     {
         println!("{}", local_scope);
-        let more_local_scope: i32 = 3;
+        let local_again: i32 = 3;
+        println!("{}", local_again); 
     }
+
    // println!("{}", more_local_scope);  // couldn't find `more_local_scope` in this scope
 }
+
+
+
+#[test]
+fn stack_heap() {
+    function_a(); 
+    function_b(); 
+}
+
+fn function_a(){
+    let a = 10;
+    let name = String::from("Muhammad"); 
+
+    println!("{}, {}", a, name); 
+}
+
+fn function_b(){
+    let a = 10;
+    let name = String::from("Ridha"); 
+
+    println!("{}, {}", a, name); 
+}
+
+
+
+
+#[test]
+#[allow(unused_assignments)]
+fn string(){
+    // this is string slice, which is stored in stack memory
+    // by default is set to immutable
+    let name: &str = "  Muhammad Ridha  "; 
+    let trim: &str = name.trim();
+
+    println!("name: &str = |{}|\ntrim: &str = |{}|", name, trim);
+    
+    // when we try to make it immutable, the data is still there the only thing change is
+    // the binding to which data in stack memory/binary form
+    let mut mutable_str: &str = "Muhammad";  
+    mutable_str = "ridha"; 
+    
+    println!("mutable_str: &str = {}", mutable_str); 
+}
+
+
+#[test]
+#[allow(unused_assignments)]
+#[allow(unused_variables)]
+#[allow(unused_mut)]
+fn string_type(){
+    let mut nama: String = String::from("Muhammad"); 
+    println!("nama: {}", nama); 
+    
+    nama.push_str(" Ridha"); 
+    println!("nama: {}", nama); 
+
+    let namabaru: String = nama.replace("Ridha", "Budi"); 
+    println!("nama: {}", namabaru); 
+}
+
+
+#[test]
+fn ownership(){
+    // variabel a tidak bisa diakses disini karena belum di deklarasikan
+    let a = 10; // variabel a bisa diakses mulai disini 
+    {
+        let b = 20; // variabel b mulai diakses dari ini 
+        println!("b = {}", b); 
+    } // variabel b selesai, dihapus & tidak bisa diakses mulai disini
+    println!("a = {}", a); 
+} // variabel a selesai, dihapus, tidak bisa diakses mulai dari sini
+
+
+#[test]
+#[allow(unused_mut)]
+fn data_copy() {
+    let mut a = 10; 
+    let mut b = a;// here rust make copy of value inside a then bind it to b 
+
+    println!("a = {}\nb = {}", a, b); 
+
+    //changing b values here won't change the value that binds to a
+    //proving that each of a and b not pointing to the same value proving that one owner at a time
+    b = 20; 
+    println!("a = {}\nb = {}", a, b); 
+}
+
+
+#[test]
+fn ownership_movement(){
+    let firstowner: String = String::from("!!!!!some heap alocated value here!!!!!"); 
+    let secondowner: String = firstowner;  // since the owner of the value is changed, firstowner
+    // is no longer valid here
+
+   println!("secondowner: {}", secondowner);  
+// this line no longer valid   println!("firstowner: {}", firstowner);  
+}
+
+
+#[test]
+fn clone(){
+    let a: String = String::from("SOME VALUE IN HELP");
+    // let b: String = a; -> this is move the ownership
+    let b: String = a.clone();
+    
+    println!("a = {}\nb = {}", a, b); 
+}
+
+
+#[test]
+#[allow(unused_variables)]
+fn if_else(){
+   let value = 1; 
+
+  // this is normal use case of fi statement
+  //  if value >= 8 {
+  //      println!("good!"); 
+  //  } else if value >= 6{
+  //      println!("not bad!"); 
+  //  } else if value >= 3{
+  //      println!("bad!"); 
+  //  }else {
+  //      println!("very bad !"); 
+  //  }
+
+// how to assing conditional value to a variable    
+//    let result: &str; 
+
+//    if value >= 8 {
+//        result = "good!" 
+//    } else if value >= 6{
+//        result = "not bad!" 
+//    } else if value >= 3{
+//        result = "bad!" 
+//    }else {
+//        result = "very bad !" 
+//    }
+
+//    println!("result is '{}' ", result); 
+      
+    let result: &str = if value >= 8 {
+        "good!" 
+    } else if value >= 6{
+        "not bad!" 
+    } else if value >= 3{
+        "bad!" 
+    }else {
+        "very bad !" 
+    }; 
+      
+    println!("result = '{}'", result); 
+}
+
+
+
+#[test]
+fn loop_expression(){
+   let mut counter = 0;
+
+    loop {
+        counter += 1; 
+        if counter > 10 {
+            break; 
+        }else if counter % 2 == 0 {
+            continue; 
+        }
+
+        println!("{}", counter);
+    }
+}
+
+#[test]
+fn loop_return_value(){
+    let mut counter = 0; 
+    let result = loop {
+        counter += 1; 
+        if counter >= 10 {
+            break counter * 2; 
+        }
+    }; 
+    println!("result: {}", result); 
+}
+
+
+#[test]
+fn loop_label() {
+    let mut number = 1;
+    'outer: loop {
+        let mut i = 1;
+        loop {
+            if number > 10 {
+                break 'outer;
+            }
+
+            println!("{} x {} = {}", number, i, number * i );
+            i += 1; 
+            if i > 10 {
+                break; 
+            }
+        }
+
+        number += 1; 
+    }
+
+}
+
+#[test]
+fn while_loop() {
+    let mut counter = 1; 
+    while counter <= 10 {
+        if counter % 2 != 0 {
+            println!("counter = {}", counter);
+        }
+        counter += 1; 
+    }
+}
+
+
+
+
+
